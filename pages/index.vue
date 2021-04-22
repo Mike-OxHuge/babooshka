db6ed513c26872fb94ab0dd8d63af03307480c9e
Mike Oxhuge <mikelitoris34@icloud.com>
2021-04-22 22:04:11 +0200

    commit on 'master' -m First commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <full-page id="fullpage" ref="fullpage" :options="options">
+    <div class="section">First section ...</div>
+    <div class="section">Second section ...</div>
+    <div class="section">Third section ...</div>
+    <div class="section">Fourth section ...</div>
+  </full-page>
+</template>
+
+<script>
+import Vue from 'vue'
+// import 'fullpage.js/vendors/scrolloverflow' // Optional. When using scrollOverflow:true
+// import './fullpage.scrollHorizontally.min' // Optional. When using fullpage extensions
+import VueFullPage from 'vue-fullpage.js'
+Vue.use(VueFullPage)
+export default {
+  components: {},
+}
+</script>
